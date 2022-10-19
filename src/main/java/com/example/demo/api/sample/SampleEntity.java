fed66736a1708c133b1b619b4a90398bf9a4ba48
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2022-10-19 16:19:50 +0700

    Update SampleEntity.java

--- src/main/java/com/example/demo/api/sample/SampleEntity.java
+++ src/main/java/com/example/demo/api/sample/SampleEntity.java
@@ -1,4 +1,28 @@
 package com.example.demo.api.sample;
 
-public class SampleEntity {
+public class SampleDto {
+    private Long id;
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public SampleDto(Sample sample) {
+        this.id = sample.getId();
+        this.name = sample.getName();
+    }
 }
