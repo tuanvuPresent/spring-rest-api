4c4ccc9c01220dc5e22f4421fd893492767883a1
vunt378 <55792941+tuanvuPresent@users.noreply.github.com>
2022-10-19 11:05:20 +0700

    Update CustomizedResponseEntityExceptionHandler.java

--- src/main/java/com/example/demo/core/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/demo/core/CustomizedResponseEntityExceptionHandler.java
@@ -1,17 +1,38 @@
 package com.example.demo.core;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
+
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ApiException.class)
     protected ResponseEntity<Object> handleApiException(ApiException ex, WebRequest request) {
-        ResponseApi responseApi = new ResponseApi(ex.message.getCode(), ex.message.getMessage());
+        ResponseApi responseApi = new ResponseApi(ex.message.getMessage(), ex.message.getCode());
         return new ResponseEntity<>(responseApi, HttpStatus.BAD_REQUEST);
     }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        ResponseApi responseApi = new ResponseApi(errors, "BAD_REQUEST");
+        return new ResponseEntity<>(responseApi, HttpStatus.BAD_REQUEST);
+    }
+
 }
